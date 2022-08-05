c3ab91e715142416818c6ca1ed85bac8e6fda3aa
GeraldTB <109708561+GeraldTB@users.noreply.github.com>
2022-08-05 09:21:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,44 @@ if(message == '!play') {
     client.say(channel, `!plays gerald22VFlorkMos`);
 }
 	
-// Aqui forzamos la respuesta cada vez que detecte en los mensajes que envian X palabra ejemplo:
-if(message.includes("!playbot")) {
+// Aqui forzamos la respuesta cada vez que detecte en los mensajes que envian X palabra ejemplo.
+if(message.includes("!mos tip 14")) {
     client.say(channel, `!play`);
 }
 
+// TheTonyBlacks
+if(message.includes("60 SECONDS TO ENTER THE ROYALE/RACE!")) {
+    client.say(channel, `!play`);
+}
+
+if(message.includes("GET YOUR BALLS ON THE TRACK, BABY! LET'S GO!")) {
+    client.say(channel, `!play`);
+}
+
+if(message.includes("GIMME YA BALLS!!")) {
+    client.say(channel, `!play`);
+}
+
+// Tokoozoona
+if(message.includes("ScaredyCat Its Time to !play")) {
+    client.say(channel, `!play`);
+}
+	
+// MerlinMilk
+if(message.includes("merlin29Hype MARBLES IN 60 SECONDS ON THE CLOCK !play GET THEM IN merlin29Hype")) {
+    client.say(channel, `!play`);
+}
+
+// MerlinMilk
+if(message.includes("merlin29Hype MARBLES IN 45 SECONDS ON THE CLOCK !play GET THEM IN merlin29Hype")) {
+    client.say(channel, `!play`);
+}
+
+	
+if(message.includes("You have 45 seconds")) {
+    client.say(channel, `!play`);
+}
+	
+	
 	
 });
